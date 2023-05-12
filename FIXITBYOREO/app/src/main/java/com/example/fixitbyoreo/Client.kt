931240ac1bc6ccd8dfa3e913fb22e17806503801
Shamry Shiraz <IT21277054@my.sliit.lk>
2023-05-13 01:38:04 +0530

    Cleaning Repo

--- FIXITBYOREO/app/src/main/java/com/example/fixitbyoreo/Client.kt
+++
@@ -1,3 +0,0 @@
-package com.example.fixitbyoreo
-
-data class Client(val title:String = "",var cusName :String = "", var jobDesc : String = "", val location : String)
--- FIXITBYOREO/app/src/main/java/com/example/fixitbyoreo/request_worker.kt
+++
@@ -1,3 +0,0 @@
-package com.example.fixitbyoreo
-
-data class request_worker(var cusName :String ?=null, var jobDesc : String ?=null)
